5cc111dcfa943cd1d9b7c1ab87fcf17896f72e54
Takayoshi Fukushima <tk44fk40@gmail.com>
2022-09-19 21:54:32 +0900

    パス修正

--- Rpi-build-FFmpeg/docker.sh
+++ Rpi-build-FFmpeg/docker.sh
@@ -11,8 +11,21 @@ if [ "$(docker ps -aq -f status=exited -f name=$dname)" ]; then
 echo Container ${dname} exist. Remove it.
   docker rm ${dname}
 fi
-mkdir -p ~/local/bin
-docker run -v '/home/rtv/local/bin:/root/bin' -it --name ${dname} ${dname} bash -c '/root/buildFF.sh'
+mkdir -p /var/tmp/local/bin
+mkdir -p /var/tmp/local/ffmpeg_build
+mkdir -p /var/tmp/local/ffmpeg_patch
+cp patch.diff /var/tmp/local/ffmpeg_patch
+docker run \
+ -v '/var/tmp/local/bin:/root/bin' \
+ -v '/var/tmp/local/ffmpeg_build:/root/ffmpeg_build' \
+ -v '/var/tmp/local/ffmpeg_patch:/root/ffmpeg_patch' \
+ --rm \
+ -it \
+ --name ${dname} \
+ ${dname} \
+ bash
+# bash -c '/root/buildFF.sh'
+
 # To change ALSA version.
 # docker run -it --env V_ALSA=1.1.9 --name ${dname} ${dname}
 #docker cp buildff:/root/bin/ffmpeg ./
