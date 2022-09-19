fd678cc5b06d855d90df6ef6a7a94d30f63dffa4
Takayoshi Fukushima <tk44fk40@gmail.com>
2022-09-19 16:31:52 +0900

    x264等コメントアウトを解除

--- Rpi-build-FFmpeg/buildFF.sh
+++ Rpi-build-FFmpeg/buildFF.sh
@@ -9,35 +9,35 @@ apt -y upgrade
 
 mkdir -p ~/ffmpeg_sources ~/bin
 
-# echo "building x264."
-# cd ~/ffmpeg_sources && \
-# git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
-# cd x264 && \
-# PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
-#   ./configure \
-#     --prefix="$HOME/ffmpeg_build" \
-#     --bindir="$HOME/bin" \
-#     --enable-static \
-#     --enable-pic && \
-# PATH="$HOME/bin:$PATH" make -j4 && \
-# make install
+echo "building x264."
+cd ~/ffmpeg_sources && \
+git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
+cd x264 && \
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
+  ./configure \
+    --prefix="$HOME/ffmpeg_build" \
+    --bindir="$HOME/bin" \
+    --enable-static \
+    --enable-pic && \
+PATH="$HOME/bin:$PATH" make -j4 && \
+make install
 
-# cd ~/ffmpeg_sources
-# echo "building mmal."
-# git clone https://github.com/raspberrypi/userland.git
-# cd userland
-# ./buildme
+cd ~/ffmpeg_sources
+echo "building mmal."
+git clone https://github.com/raspberrypi/userland.git
+cd userland
+./buildme
 
-# cd ~/ffmpeg_sources
-# echo "building alsa."
-# git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg --depth 1
-# wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-$V_ALSA.tar.bz2
-# tar xjvf alsa-lib-$V_ALSA.tar.bz2
-# cd /home/alsa-lib-$V_ALSA
-# PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
-#   ./configure --prefix="$HOME/ffmpeg_build" \
-# && PATH="$HOME/bin:$PATH" make -j4 \
-# && make install
+cd ~/ffmpeg_sources
+echo "building alsa."
+git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg --depth 1
+wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-$V_ALSA.tar.bz2
+tar xjvf alsa-lib-$V_ALSA.tar.bz2
+cd /home/alsa-lib-$V_ALSA
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
+  ./configure --prefix="$HOME/ffmpeg_build" \
+&& PATH="$HOME/bin:$PATH" make -j4 \
+&& make install
 
 export LD_LIBRARY_PATH="/opt/vc/lib"
 cd ~/ffmpeg_sources \
